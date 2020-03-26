8626cc087bf6043a11f75eeb14776c3c8fcbb122
HackerStudy <1791133899@qq.com>
2020-03-26 22:36:57 +0800

    jdk源码

---
+++ src/test/java/hashmap/Test.java
@@ -0,0 +1,19 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @class: Test
+ * @description: 调试hashmap源码
+ * @author: 17911
+ * @date: 2020-03-26 21:56
+ */
+public class Test {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<>();
+        map.put("key1",1);
+        map.put("key2",2);
+        map.put("key3",3);
+    }
+}
